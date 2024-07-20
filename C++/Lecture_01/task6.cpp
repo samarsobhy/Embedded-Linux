f970a0f5beb0314bdb2c6d76b0d7497181195b7d
Samar Sobhy <83344314+samarsobhy@users.noreply.github.com>
2024-07-20 15:39:19 +0300

    Add files via upload

---
+++ C++/Lecture_01/task6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    std::string input;
+    int sum = 0;
+
+    std::cout << "Enter a number: ";
+    std::cin >> input;
+    
+    for (char ch : input)
+    {
+        if (std::isdigit(ch))
+        {
+            sum += ch - '0' ; //convert it to the corresponding integer value by subtracting the ASCII value of '0' (which is 48) from the ASCII value of ch.
+                              //If ch is '1', then ch - '0' is 49 - 48 which equals 1.
+        }
+        else
+        {
+            std::cout <<"Warning: Non-digit characters were ignored"<<std::endl;
+        }
+    }   
+    
+    std::cout <<"The sum of digits of "<<input<<" is "<<sum<<std::endl;
+   
+    return 0;
+}
